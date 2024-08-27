3807008a2a4bbe84526d50d0cc8b83f2442ba18e
Caden LeCluyse <lecluyse20@gmail.com>
2024-08-26 21:49:49 -0500

    Added a config file for wezterm
    Disabled wayland because apparently it doesn't work right now

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- This is where you actually apply your config choices
+config.enable_wayland = false
+
+-- and finally, return the configuration to wezterm
+return config
